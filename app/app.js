5a783b241feaa134be6ee3a3cd2dd461bbbe96c6
Jon Bitler <jbitler@jons-mbp.atlanta.beckshybrids.com>
2015-09-28 15:21:34 -0400

    moved 'notes' state config into seperate file

--- app/app.js
+++ app/app.js
@@ -1,22 +1,16 @@
 (function() {
   var app = angular.module('notely', [
-    'ui.router'
+    'ui.router',
+    'notely.notes'
   ]);
 
-  function config($stateProvider, $urlRouterProvider) {
+  function config($urlRouterProvider) {
     $urlRouterProvider.otherwise('/notes');
 
-    $stateProvider
-
-      .state('notes', {
-        url: '/notes',
-        template: '<h1>Notely</h1><p>{{ message }}</p>',
-        controller: function($scope) {
-          $scope.message = "Welcome to Notely!";
-        }
-      });
   }
 
-  config['$inject'] = ['$stateProvider', '$urlRouterProvider'];
+  //more like angular 2
+  //should always do this manually instead of ng-annotate
+  config['$inject'] = ['$urlRouterProvider'];
   app.config(config);
 })();
